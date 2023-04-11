5d11b08b12e9dca5987e45392dd8bff1fa2b79fc
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2023-04-10 18:55:29 -0600

    Se maqueto y dio funcionalidad a la calculadora

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -7,9 +7,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  displayNumber: any = '0';
+  displayCalculation: any = '';
+  answer = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  clickNumberBtn(num: any) {
+    if(this.answer != ''){
+      this.clear();
+    }
+    if ((this.displayNumber == 0 || this.displayNumber == '0') && (num != '+' && num != '-' && num != '*' && num != '/')) {
+      this.displayNumber = num;
+    } else {
+      if (num === '+' || num === '-' || num === '*' || num === '/') {
+        this.displayCalculation += this.displayNumber + num;
+        this.displayNumber = '0';
+      } else {
+        this.displayNumber += num
+      }
+    }
+  }
+
+  clear() {
+    this.displayNumber = '0';
+    this.displayCalculation = '';
+    this.answer = '';
+  }
+
+  deleteByOne() {
+    if(this.displayNumber.length > 1){
+      this.displayNumber = this.displayNumber.substring(0,this.displayNumber.length - 1);
+    } else {
+      this.displayNumber = '0';
+    }
+  }
+
+  evaluate() {
+    this.displayCalculation += this.displayNumber;
+    this.answer = eval(this.displayCalculation);
+    console.log(this.answer);
+    this.displayNumber = '0';
+  }
+
+  ////
+  // displayValue = '';
+
+
+  // handleInput(value: string) {
+  //   if (value === '+' || value === '-' || value === '*' || value === '/') {
+  //     this.calculation += this.displayValue + value;
+  //     this.displayValue = '';
+  //   } else {
+  //     this.displayValue += value;
+  //   }
+  // }
+
+  // calculate() {
+  //   this.calculation += this.displayValue;
+  //   this.displayValue = eval(this.calculation);
+  //   this.calculation = '';
+  // }
 
 }
